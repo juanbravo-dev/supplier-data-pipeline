5c7d48bac805e9e920a9a6602dce6468cb53215a
Juan D. Bravo <196306536+juanbravo-dev@users.noreply.github.com>
2025-04-28 06:53:58 +0200

    feat: add logging functionnality to extract.py

--- extract/extract.py
+++ extract/extract.py
@@ -2,20 +2,29 @@
 Extraction of first montly CSV report.
 
 """
-
+import logging
 import pandas as pd
 from pathlib import Path
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s - %(levelname)s - %(message)s"
+)
 
 DATA_DIR = Path("data")
 OUTPUT_DIR = Path("output")
 OUTPUT_DIR.mkdir(parents=True, exist_ok=True)
 
 def read_csv(input_path: Path) -> pd.DataFrame:
+    logging.info(f"Reading file: {input_path}")
     df = pd.read_csv(input_path, encoding='latin1', delimiter=';', decimal=',')
+    logging.info(f"File {input_path.name} read successfully with shape {df.shape}")
     return df
 
 def save_csv(df: pd.DataFrame, output_path: Path):
+    logging.info(f"Saving extracted data to: {output_path}")
     df.to_csv(output_path, index=False)
+    logging.info(f"💾 Data saved successfully to {output_path}")
 
 def main():
     month_name = "january"
@@ -28,7 +37,9 @@ def main():
 
     df = read_csv(input_file)
     save_csv(df, output_file)
+    logging.info(f"✅ Extraction finished for {month_name.upper()}")
 
 if __name__ == "__main__":
+    
     main()
 
