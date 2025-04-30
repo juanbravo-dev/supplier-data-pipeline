bb071167338bd541df8e4749e5d5a87a62c638a5
Juan D. Bravo <196306536+juanbravo-dev@users.noreply.github.com>
2025-04-30 06:53:07 +0200

    feat: log output to both console and transform.log file

--- transform/transform.py
+++ transform/transform.py
@@ -1,11 +1,23 @@
 
 import pandas as pd
 from pathlib import Path
+import logging
+import sys
 
 INPUT_DIR = Path("output")
 OUTPUT_DIR = Path("cleaned")
 OUTPUT_DIR.mkdir(parents=True, exist_ok=True)
 
+LOG_FILE = "transform.log"
+
+logging.basicConfig(
+    level= logging.INFO,
+    format= "%(asctime)s - %(levelname)s - %(message)s",
+    handlers=[
+        logging.StreamHandler(sys.stdout),
+        logging.FileHandler(LOG_FILE)
+    ]
+)
 
 def clean_data (df: pd.DataFrame) -> pd.DataFrame:
     """
@@ -22,16 +34,21 @@ def clean_data (df: pd.DataFrame) -> pd.DataFrame:
 
 
 
-
-
 if __name__ == "__main__":
     month = "january"  # hardcoded for now
+
+    logging.info(f"Starting transformation for: {month}")
 
     input_file = INPUT_DIR / f"raw_extracted_{month}.csv"
     output_file = OUTPUT_DIR / f"cleaned_transformed_{month}.csv"
 
     df_raw = pd.read_csv(input_file)
-    df_clean = clean_data(df_raw)
-    df_clean.to_csv(output_file, index=False)
+    logging.info(f"Loaded file: {input_file.name} with shape {df_raw.shape}")
 
+    df_clean = clean_data(df_raw)
+    logging.info("Data cleaned successfully.")
+
+    df_clean.to_csv(output_file, index=False)
+    logging.info(f"Saved cleaned file to: {output_file}")
+    
     print(f"✅ Cleaned file saved to: {output_file}")
